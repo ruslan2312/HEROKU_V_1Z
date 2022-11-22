2dd1b83aabadf93f98956628d3181b6a0064fea4
ruslan2312 <heeca@mail.ru>
2022-11-22 15:53:20 +0300

    Fix delete:deviceId

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -35,7 +35,7 @@ authRouter.post('/login', responseCountMiddleware, authLoginValidation, inputVal
         await deviceService.addDevice(user.id, req.headers["user-agent"]!, req.ip, deviceId, time.iat, time.exp)
         res.cookie("refreshToken", token.refreshToken, {
                 httpOnly: true,
-                secure: false
+                secure: true
             },
         ).status(200).send({accessToken: token.accessToken})
     } else {
@@ -81,7 +81,7 @@ authRouter.post('/refresh-token', checkUsersByRefreshToken, inputValidationMiddl
         const token = await jwtService.createJWT(user, payload.deviceId!)
         res.cookie("refreshToken", token.refreshToken, {
                 httpOnly: true,
-                secure: false
+                secure: true
             },
         ).send({accessToken: token.accessToken}).status(200)
         const payload2: any = await deviceService.getPayload(token.refreshToken)
