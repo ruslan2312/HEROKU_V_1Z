52e3de421055fc6f067d467aa244fdf3cd925ff9
ruslan2312 <heeca@mail.ru>
2022-12-17 00:40:25 +0300

    fix findPostId PagesCount

--- src/repository/blogs-repository.ts
+++ src/repository/blogs-repository.ts
@@ -30,9 +30,8 @@ export const blogsRepository = {
         return BlogsModel.findOne({id: id}, {_id: 0, __v: 0});
     },
     async findBlogByPostId(queryData: FindPostByIdPaginationQueryType, blogId: string, userId: string): Promise<PaginationResultType | null> {
-        console.log(blogId)
         const objectSort = {[queryData.sortBy]: queryData.sortDirection}
-        const totalCount = await PostsModel.countDocuments({});
+        const totalCount = await PostsModel.countDocuments({blogId: blogId});
         const page = Number(queryData.pageNumber)
         const pageSize = Number(queryData.pageSize)
         const posts = await PostsModel.aggregate([
@@ -102,7 +101,6 @@ export const blogsRepository = {
             .sort(objectSort)
             .skip((page - 1) * pageSize)
             .limit(pageSize)
-        console.log(posts)
         return paginationResult(page, pageSize, totalCount, posts)
     },
     async createBlog(newBlog: BlogsType): Promise<BlogsType> {
